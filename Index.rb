7e51b907a78ecff6a84be7f11a0b3487586b2206
Steve-Drewery <99699722+Steve-Drewery@users.noreply.github.com>
2022-04-12 06:39:53 +1000

    cant figure out the menu, need sleep

--- Index.rb
+++ Index.rb
@@ -2,23 +2,13 @@ require 'colorize'
 require "tty-prompt"
 require_relative("./drivers.rb")
 require_relative("./tracks.rb")
-circuit = Tracks
-
- puts circuit
- puts circuit.print_tracks
-
 $prompt = TTY::Prompt.new
-$Drivers = Driver
 
 def select_option
-    answer = $prompt.select("Welcome to Formula 1".red,["Table","Season Races","Drivers","Teams","Exit"])
+    answer = $prompt.select("Welcome to Formula 1".red,["Table","Season Races","Drivers","Teams","Exit",],cycle: true)
     answer
 end
 
-def select_driver
-    driver = $prompt.select("Select Driver",$Drivers.array_drivers1)
-    return driver
-end
 system "clear"
 puts "Formula 1".red
 option =""
@@ -29,9 +19,9 @@ while option != "Exit"
         print "Table"
     when "Season Races"
         print "Season Races"
+        $prompt.select("Formula ".red,["Races 1-5","Races 6-10","Races 11-15","Races 16-20","Races 20-22"])
     when "Drivers"
-        print "Driver List"
-        puts array_drivers1
+        puts "Driver List"
     when "Teams"
         print "Teams2"
     else
